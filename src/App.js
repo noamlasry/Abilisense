63a512ac1fe07bc1cd4a4c034435012cfb7e1005
amir <amirbesig@gmail.com>
2019-12-10 18:03:05 +0200

    df

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <code></code>we are the coders!
+          <code></code>we ar
         </p>
         <a
           className="App-link"
