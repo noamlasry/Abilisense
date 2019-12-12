26c2f46096a12cb0c1293e0a00f82d5fc08cadc6
amir <amirbesig@gmail.com>
2019-12-12 09:32:50 +0200

    npm installed

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Login from "./components/login";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-<<<<<<< HEAD
-          <code></code>we ar
-=======
-          <code></code>blablablaaaa
->>>>>>> 8141d15cc912583d7ae344a93ebd8cb940878402
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  ntDate = "20/21/2020";
+    render(){
+       return(
+       <div className="App">
+        <Login></Login>
+      
+      </div>
+    )
+};
 }
 
 export default App;
---
+++ src/components/navBar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class NavBar extends Component{
+    render(){
+        return(
+            <div>
+               nav bar
+            </div>
+        )
+    }
+}
+export default navBar;
