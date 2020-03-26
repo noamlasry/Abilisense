1383db3f2a2bfabd13ca65caa1014d0adb861364
Arkanoid <noamlasry02@gmail.com>
2020-03-26 08:48:06 +0200

    organize media player files

--- src/components/mediaPlayer/MusicPlayer.js
+++ src/components/mediaPlayer/MusicPlayer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import Progress from '../Progress';
+import Progress from './Progress';
 import './MusicPlayer.scss';
 
 const formatTime = time => {
