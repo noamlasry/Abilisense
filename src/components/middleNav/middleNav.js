97528b873bcbdc2a9fd6009fefd28f2bfb4619f9
amir <amirbesig@gmail.com>
2020-03-28 09:55:08 +0300

    ui minus player

--- src/components/middleNav/middleNav.js
+++ src/components/middleNav/middleNav.js
@@ -1,29 +1,7 @@
 import React, { Component } from "react";
 import './middleNav.css';
 import {ButtonToolbar, Button} from "react-bootstrap";
-import MusicPlayer from "../mediaPlayer/MusicPlayer";
-
-
-
-const playlist = [
-  {
-    url: 'http://res.cloudinary.com/alick/video/upload/v1502689683/Luis_Fonsi_-_Despacito_ft._Daddy_Yankee_uyvqw9.mp3',
-    cover: 'path/to/jpg',
-    title: 'Despacito',
-    artist: [
-      'Luis Fonsi',
-      'Daddy Yankee'
-    ]
-  },
-  {
-    url: 'http://res.cloudinary.com/alick/video/upload/v1502375674/Bedtime_Stories.mp3',
-    cover: 'path/to/jpg',
-    title: 'Bedtime Stories',
-    artist: [
-      'Jay Chou'
-    ]
-  }
-]
+import Player from "../mediaPlayer/player";
 
 class MiddleNav extends Component {
     render(){
@@ -31,7 +9,7 @@ class MiddleNav extends Component {
       return(
         <div className="middlenav">
             <h2 className="label">Audio Editor</h2>
-            <MusicPlayer playlist={playlist} />
+            <Player />
               <ButtonToolbar className="btnTool">
                 <Button className="btn" variant="outline-primary">Audio tag</Button>
                 <Button className="btn" variant="outline-primary">Audio tag</Button>
