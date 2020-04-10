7b963563c08380d216d59759091ce7c9a94f329b
amir <amirbesig@gmail.com>
2020-04-10 17:55:54 +0300

    updated react scripts

--- src/components/leftNav/leftNav.js
+++ src/components/leftNav/leftNav.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import './leftNav.css';
-// import  Category  from "../categories/category";
 import { MDBContainer } from "mdbreact";
-
 import { Storage } from "@aws-amplify/storage";
 // import { S3Image } from 'aws-amplify-react';
-
 
 class LeftNav extends Component {
     state = {
@@ -18,46 +15,11 @@ class LeftNav extends Component {
       }
     
     
-    // state = {fileUrl: '', file:'', filename:''}
-    // handlechange = e => {
-    //     const file = e.target.files[0]
-    //     this.setState({
-    //         fileUrl: URL.createObjectURL(file),
-    //         file,
-    //         filename: file.name
-    //     })
-    // }
-    // savefile = () => {
-    //     Storage.put(this.state.filename, this.state.file)
-    //         .then(() => {
-    //             console.log("success upload")
-    //             this.setState({fileUrl: '', file: '', filename: ''})
-    //         })
-    //         .catch(err =>{
-    //             console.log('theres an error amir!!!!', err)
-    //         })
-    // }
-    
-    
     render(){
         const scrollContainerStyle = { width: "100%", maxHeight: "80vh" };
-
         return(
-            
-
             <div className="leftnav">
                 <h3 className="h3" >Categories</h3>
-
-            {/* {
-                this.state.files.map((f, i) => (
-                    <div key={i}>
-                        <S3Image 
-                        imgKey={f.key}
-                        />
-                        <h3>f.key</h3>
-                    </div>
-                ))
-            } */}
             <MDBContainer>
                 <div className="scrollbar scrollbar-primary"  style={scrollContainerStyle}>
                 <ol className="ol" >
@@ -68,7 +30,6 @@ class LeftNav extends Component {
                 </ol>
                 </div>
               </MDBContainer>
-                {/* <Category /> */}
             </div>
         )
  };
