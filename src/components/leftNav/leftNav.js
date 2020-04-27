56ee9b7d8cc408c6842eabefe2fdefb943c345af
noam <noamlasry02@gmail.com>
2020-04-27 20:14:29 +0300

    clean left nav unnecessarly code

--- src/components/leftNav/leftNav.js
+++ src/components/leftNav/leftNav.js
@@ -10,7 +10,7 @@ class LeftNav extends Component {
     super(props);
   
     this.findSubCategory = this.findSubCategory.bind(this);
-    this.validAndUnvalid = this.validAndUnvalid.bind(this);
+   
 
   }
 
@@ -24,31 +24,21 @@ class LeftNav extends Component {
       }
 
       async componentDidMount() {
-        
         const files = await Storage.list('')
         this.setState({ files })
         this.findSubCategory();
       }
  
-      validAndUnvalid()
-      {
-
-      }
-      
+ 
       findSubCategory() {
         var i;
         var folder = [];
         for(i = 0; i<this.state.files.length; i++)
         {
-         
           var item = this.state.files[i].key;
           var keyNameEnd = item.slice(-1);
-          var KeyName = item.slice(0,item.length-1);
           if(keyNameEnd === '/')
             folder.push(this.state.files[i]);
-          
-            
-         
         }
         this.setState({folder})
       }
@@ -57,37 +47,30 @@ class LeftNav extends Component {
     render(){
   
         return(
-          <div className="leftnav">
-            <Accordion>
-  <Card>
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="0">
-        Valid
-      </Accordion.Toggle>
-    </Card.Header>
-       {this.state.folder.map((f,i) =>
-                 <div className="li" key={i}>
-                    <Accordion.Collapse eventKey="0">
+            <Accordion className="leftnav">
+             <Card>
+               <Card.Header>
+                 <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                 Valid
+                 </Accordion.Toggle>
+               </Card.Header>
+                    {this.state.folder.map((f,i) =>
+                    <Accordion.Collapse eventKey="0" className="li" key={i}>
                     <Card.Body> {f.key.slice(0,f.key.length-1)}</Card.Body>
-                    </Accordion.Collapse>
-                </div>)}
-  </Card>
-  <Card>
-    <Card.Header>
-      <Accordion.Toggle as={Button} variant="link" eventKey="1">
-        Unvalid
-      </Accordion.Toggle>
-    </Card.Header>
-    {this.state.folder.map((f,i) =>
-                 <div className="li" key={i}>
-                    <Accordion.Collapse eventKey="1">
-                    <Card.Body> {f.key.slice(0,f.key.length-1)}</Card.Body>
-                    </Accordion.Collapse>
-                </div>)}
-  </Card>
-</Accordion>
-
-          </div>
+                    </Accordion.Collapse>)}
+             </Card>
+             <Card>
+               <Card.Header>
+                 <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                 Unvalid
+                 </Accordion.Toggle>
+               </Card.Header>
+                   {this.state.folder.map((f,i) =>
+                   <Accordion.Collapse eventKey="1" className="li" key={i}>
+                   <Card.Body> {f.key.slice(0,f.key.length-1)}</Card.Body>
+                   </Accordion.Collapse>)}
+             </Card>
+           </Accordion>
         )
  };
    
