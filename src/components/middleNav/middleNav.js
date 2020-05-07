651ea18c86469743831f8e46f99a4a4118d33544
noam <noamlasry02@gmail.com>
2020-05-07 18:08:30 +0300

    remove aws request from middleNave

--- src/components/middleNav/middleNav.js
+++ src/components/middleNav/middleNav.js
@@ -1,37 +1,25 @@
 import React, { Component } from "react";
 import './middleNav.css';
 import {ButtonToolbar, Button} from "react-bootstrap";
+import MusicPlayer from "../mediaPlayer/MusicPlayer"
 import { Storage } from "@aws-amplify/storage";
-import AudioPlayer from 'react-h5-audio-player';
-import 'react-h5-audio-player/lib/styles.css';
 
 
 
 class MiddleNav extends Component {
   state = {     
     files: [],
-    lists:[ { }  ],
-    temp:[{}]
-
+    lists:[ { }  ]
 
   }
-  async componentDidMount() {
-    const files = await Storage.list('')
-    this.setState({ files })
+   componentDidMount() {
+ 
     
-   let i;
-   for(i =0; i<files.length; i++)
-   {
-     const audioUrl = await Storage.get(files[i].key);
-     const objectKey = files[i].key;
-     this.state.lists.push({url:audioUrl,title:objectKey});
-   }
-     
-      this.state.lists.shift();
-      this.state.lists.shift();
+ 
+    this.state.lists.push({url:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',title:'song-1'});   
+    this.state.lists.push({url:'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3',title:'song-2'});   
 
-      console.log(this.state.lists);
-     
+      this.state.lists.shift();
   }
  
     render(){
@@ -39,12 +27,7 @@ class MiddleNav extends Component {
       return(
         <div className="middlenav">
             <h2 className="label">Audio Player</h2>
-            <AudioPlayer
-    autoPlay
-    src="https://www.youtube.com/watch?v=Oioo0IdoEls"
-    onPlay={e => console.log("onPlay")}
-    // other props here
-  />
+            <MusicPlayer playlist={ this.state.lists}  autoPlay={true}/>
               <ButtonToolbar className="btnTool">
                 <Button className="btn" variant="outline-primary">Audio tag</Button>
                 <Button className="btn" variant="outline-primary">Audio tag</Button>
